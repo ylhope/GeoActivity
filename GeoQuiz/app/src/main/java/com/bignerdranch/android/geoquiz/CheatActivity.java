fc5016f99a2572bb8732ae7c44967cb348d7d42b
ylhope <858131544@qq.com>
2017-07-24 17:57:00 +0800

    旋转漏洞的补充
    之前该存的参数错了.分析之后改好了

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,8 +13,9 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
-//    private static final String KEY_INDEX = "index";
     private static final String KEY_CLICK = "false";
+    private static final String TAG ="CheatActivity";
+    private boolean mIsCheat = false;
 
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
@@ -42,29 +44,57 @@ public class CheatActivity extends AppCompatActivity {
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(savedInstanceState != null){
-                    mAnswerIsTrue = savedInstanceState.getBoolean(KEY_CLICK, false);
-                }
+                mIsCheat = true;
                 if(mAnswerIsTrue){
                     mAnswerTextView.setText(R.string.true_button);
                 }else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
-                setAnswerShownResult(true);
+                setAnswerShownResult(mIsCheat);
             }
         });
+        if(savedInstanceState != null){
+            mIsCheat = savedInstanceState.getBoolean(KEY_CLICK, false);
+            setAnswerShownResult(mIsCheat);
+        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
-//        Log.
-        savedInstanceState.putBoolean(KEY_CLICK, mAnswerIsTrue);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(KEY_CLICK, mIsCheat);
     }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK, data);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "CheatActivity onStart() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "CheatActivity onPause() called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"CheatActivity onResume() called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"CheatActivity onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "CheatActivity onDestroy() called");
     }
 }
--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -156,29 +156,29 @@ public class QuizActivity extends AppCompatActivity {
         savedInstanceState.putBoolean(KEY_CHEATER,mIsCheater);
     }
 
-//    @Override
-//    public void onStart(){
-//        super.onStart();
-//        Log.d(TAG, "onStart() called");
-//    }
-//    @Override
-//    public void onPause(){
-//        super.onPause();
-//        Log.d(TAG, "onPause() called");
-//    }
-//    @Override
-//    public void onResume(){
-//        super.onResume();
-//        Log.d(TAG,"onResume() called");
-//    }
-//    @Override
-//    public void onStop(){
-//        super.onStop();
-//        Log.d(TAG,"onStop() called");
-//    }
-//    @Override
-//    public void onDestroy(){
-//        super.onDestroy();
-//        Log.d(TAG, "onDestroy() called");
-//    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
 }
